37374279bcfe739af6739e621a52da6c777faf70
Hkashyap <hkashyap.developer@gmail.com>
2023-08-12 18:15:02 +0530

    Commit App

---
+++ src/Global/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="testHeader">
+      <h2>Header test</h2>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>HOME PAGE NEW</h2>
+    </div>
+  )
+}
+
+export default Home
