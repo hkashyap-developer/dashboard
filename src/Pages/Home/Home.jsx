257a4f9f9115276155ccabc2ace0eea1dd3779f0
Hkashyap <hkashyap.developer@gmail.com>
2023-08-14 16:48:31 +0530

    Home Page Header

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BackgroundImg from "../../Media/Images/home-background.jpg"
 import './Home.css'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -13,7 +14,9 @@ const Home = () => {
 
     <div className="heroBannerotrBx">
       <div className="heroBnrInrBx" style={{ backgroundImage: `url(${BackgroundImg})` }}>
-        <h1>You got the travel plans, we got the travel vans.</h1>
+        <h1 className="heroHeading">You got the travel plans, we got the travel vans.</h1>
+        <p className="heroDesc">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+        <button className="heroBtn"><Link to="/about" className="heroBtnLink">Find your van</Link></button>
       </div>
     </div>
   )
