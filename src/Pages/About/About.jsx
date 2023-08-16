66c4e6554eeae00ffa5ba07fddddc6f4deb1104b
Hkashyap <hkashyap.developer@gmail.com>
2023-08-16 06:37:03 +0530

    Home Page done.

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    
+    <img className="headImageAbt" src="" />
+
+    <div>
+      <h1>About Page New</h1>
+    </div>
+  )
+}
+
+export default About
