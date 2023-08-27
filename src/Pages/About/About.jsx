93eb716f48af0f3797b70d1403b6498651807555
Hkashyap <hkashyap.developer@gmail.com>
2023-08-28 04:37:35 +0530

    About page done.

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div class="abtHero-inrBx">
         <div class="abtHero-r1">
           <div class="abtHero-col1 abtHero-col">
-            <h1>The last app to manage all your important links.</h1>
+            <h1>The last app to manage all your important 🔗links.</h1>
             <p>"Introducing our innovative web app that empowers you to effortlessly manage links and create dynamic dashboards. Streamline your link organization by categorizing and tagging URLs."</p>
           </div>
           <div class="abtHero-r2 abtHero-col">
